0d31220dcb9ab0d49b13346845c88b32109d9a8c
hanifcs <hanif.ullah007@gmail.com>
2021-07-19 23:03:49 +0500

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,12 +47,47 @@ class _HomePageState extends State<HomePage> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: (CatalogModel.items.isNotEmpty)
-                ? ListView.builder(
-                    itemCount: CatalogModel.items.length,
-                    itemBuilder: (BuildContext context, int index) =>
-                        ItemWidget(
-                      item: CatalogModel.items[index],
+                ? GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
                     ),
+                    itemBuilder: (context, index) {
+                      final item = CatalogModel.items[index];
+                      return Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: GridTile(
+                            child: Image.network(item.image),
+                            header: Container(
+                              child: Text(
+                                item.name,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(12.0),
+                              decoration: BoxDecoration(
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                            footer: Container(
+                              child: Text(
+                                item.price.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(12.0),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ));
+                    },
+                    itemCount: CatalogModel.items.length,
                   )
                 : Center(
                     child: CircularProgressIndicator(),
